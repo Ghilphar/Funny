4d28bb88dbdbca1cbcf442bbf871c0af4ee8935f
FGaribotZento <felipe.garibotti@zento.fr>
2023-07-05 00:26:04 +0200

    ajout correct de l'initialisation de la db ! correction du folder docker node

--- backend/index.js
+++ backend/index.js
@@ -60,23 +60,23 @@ db.connect((err) => {
     });
 });
 
-db.connect((err) => {
-    if (err) throw err;
-    console.log('Connected to the database');
-    let sql = `
-        CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            firstname VARCHAR(255) NOT NULL,
-            lastname VARCHAR(255) NOT NULL,
-            username VARCHAR(255) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL
-        )`;
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log('Users table created or already exists');
-    });
-});
+//db.connect((err) => {
+//    if (err) throw err;
+//    console.log('Connected to the database');
+//    let sql = `
+//        CREATE TABLE IF NOT EXISTS users (
+//            id INT AUTO_INCREMENT PRIMARY KEY,
+//            firstname VARCHAR(255) NOT NULL,
+//            lastname VARCHAR(255) NOT NULL,
+//            username VARCHAR(255) UNIQUE NOT NULL,
+//            email VARCHAR(255) UNIQUE NOT NULL,
+//            password VARCHAR(255) NOT NULL
+//        )`;
+//    db.query(sql, (err, result) => {
+//        if (err) throw err;
+//        console.log('Users table created or already exists');
+//    });
+//});
 
 
 
